d83a696e4a176f33cc7492c4042c9043848c58e7
buhari36 <buharihassan36@example.com>
2023-11-15 16:34:45 +0100

    emailjs import added

--- src/pages/partyPlan/RequestY.jsx
+++ src/pages/partyPlan/RequestY.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./requesty.css";
+import emailjs from "emailjs-com";
 
 const RequestY = () => {
   const [enteredFirstName, setEnteredFirstName] = useState("");
